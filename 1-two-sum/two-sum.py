9b1fac067e14c8415aa1cdf651691ba7016277d3
Chappa Bharath kumar <chappabharathkumar8@gmail.com>
2026-07-19 14:14:51 +0530

    Time: 0 ms (100.00%) | Memory: 20.6 MB (7.00%) - LeetSync

--- 1-two-sum/two-sum.py
+++ 1-two-sum/two-sum.py
@@ -1,8 +1,12 @@
 class Solution:
-    def twoSum(self, nums, target):
-        seen = {}  # value -> index
-        for i, num in enumerate(nums):
-            complement = target - num
-            if complement in seen:
-                return [seen[complement], i]
-            seen[num] = i
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        seen={}
+
+        for i in range(len(nums)):
+            need = target - nums[i]
+
+            if need in seen:
+                return seen[need],i
+            else:
+                seen[nums[i]]=i
+        
